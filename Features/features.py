e2220d9394c12add58e73c16ae2a8544b2064066
ghanadparisa@gmail.com <ghanadparisa@gmail.com>
2022-05-11 23:51:51 +0430

    l

--- Features/features.py
+++ Features/features.py
@@ -4,62 +4,62 @@ import numpy as np
 #### FLAG FUNCTIONS ####
 def fin_flag_counts(packets):
     counts = 0
-    for packet in flow.get_packets():
+    for packet in packets:
         if packet.has_flagFIN():
             counts+=1
     return counts
 
 def psh_flag_counts(packets):
     counts = 0
-    for packet in flow.get_packets():
+    for packet in packets:
         if packet.has_flagPSH():
             counts+=1
     return counts
             
 def urg_flag_counts(packets):
     counts = 0
-    for packet in flow.get_packets():
+    for packet in packets:
         if packet.has_flagURG():
             counts+=1
     return counts
 
 def ece_flag_counts(packets):
     counts = 0
-    for packet in flow.get_packets():
+    for packet in packets:
         if packet.has_flagECE():
             counts+=1
     return counts
             
 def syn_flag_counts(packets):
     counts = 0
-    for packet in flow.get_packets():
+    for packet in packets:
         if packet.has_flagSYN():
             counts+=1
     return counts
             
 def ack_flag_counts(packets):
     counts = 0
-    for packet in flow.get_packets():
+    for packet in packets:
         if packet.has_flagACK():
             counts+=1
     return counts
             
 def cwr_flag_counts(packets):
     counts = 0
-    for packet in flow.get_packets():
+    for packet in packets:
         if packet.has_flagCWR():
             counts+=1
     return counts
             
 def rst_flag_counts(packets):
     counts = 0
-    for packet in flow.get_packets():
+    for packet in packets:
         if packet.has_flagRST():
             counts+=1
     return counts
             
 ###############################
-### TIME ####
+
 def flow_duration(flow):
     return flow.get_flow_last_seen() - flow.get_flow_start_time()
 
@@ -118,3 +118,4 @@ def flow_packets_length_std(packets):
         return np.std(packets_len)
     return 0
 #################################
+
